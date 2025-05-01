0a6a4452c16fe7d7b681bbf00b1a47bd40fd08c7
Tamas Karoly <tamas.karoly.mail@gmail.com>
2025-05-01 12:12:33 +0200

    Changed IOCTL fn so that it always completes the incoming IRP.

--- TerrorDriver/src/device.c
+++ TerrorDriver/src/device.c
@@ -3,7 +3,6 @@
 #include "device.h"
 #include "dbgprint.h"
 
-
 NTSYSAPI NTSTATUS NTAPI MmCopyVirtualMemory(PEPROCESS FromProcess, PVOID FromAddress, PEPROCESS ToProcess, PVOID ToAddress, SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode, PSIZE_T ReturnSize);
 
 NTSYSCALLAPI NTSTATUS NTAPI ZwGetNextThread(_In_ HANDLE ProcessHandle, _In_ HANDLE ThreadHandle, _In_ ACCESS_MASK DesiredAccess, _In_ ULONG HandleAttributes, _In_ ULONG Flags, _Out_ PHANDLE NewThreadHandle);
@@ -24,7 +23,8 @@ NTSYSCALLAPI NTSTATUS NTAPI ZwGetNextThread(_In_ HANDLE ProcessHandle, _In_ HAND
 #define PsGetThreadTeb(Thread) ((*(PVOID *)((PUCHAR)(Thread) + 0x0e0)))
 #endif
 
-
+//#define THREAD_GET_CONTEXT 0x0008
+#define THREAD_QUERY_INFORMATION 0x0040
 
 NTSTATUS CreateCall(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
@@ -63,186 +63,179 @@ NTSTATUS IOControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 	ULONG ByteIo = 0;
 
 	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
-
 	ULONG ControlCode = stack->Parameters.DeviceIoControl.IoControlCode;
 
-	if (ControlCode == IO_UNMAP_NTDLL)
+	do
 	{
-		
-		DWORD pid = *(DWORD*)Irp->AssociatedIrp.SystemBuffer;
-
-		DebugPrint("Control code 0x111 was passed with pid: %u\n", pid);
-		
-
-		PVOID ntdll = GetNtDll(pid);
-
-
-		DebugPrint("ntdll address: %p\n", ntdll);
-
-
-		PEPROCESS pEProcess = NULL;
-		HANDLE handle = NULL;
-
-		status = PsLookupProcessByProcessId((HANDLE)pid, &pEProcess);
-		if (!NT_SUCCESS(status)) 
+		if (ControlCode == IO_UNMAP_NTDLL)
 		{
-			DebugPrint("[-] PsLookupProcessByProcessId failed: 0x%X\n", status);
-			return status;
+
+			DWORD pid = *(DWORD*)Irp->AssociatedIrp.SystemBuffer;
+
+			DebugPrint("Control code 0x111 was passed with pid: %u\n", pid);
+
+			PVOID ntdll = GetNtDll(pid);
+
+			DebugPrint("ntdll address: %p\n", ntdll);
+
+			PEPROCESS pEProcess = NULL;
+			HANDLE handle = NULL;
+
+			status = PsLookupProcessByProcessId((HANDLE)pid, &pEProcess);
+			if (!NT_SUCCESS(status))
+			{
+				DebugPrint("[-] PsLookupProcessByProcessId failed: 0x%X\n", status);
+				break;
+			}
+
+			status = ObOpenObjectByPointer(pEProcess, OBJ_KERNEL_HANDLE, NULL, 0x0008, *PsProcessType, KernelMode, &handle); //PROCESS_VM_OPERATION
+
+			if (!NT_SUCCESS(status))
+			{
+				DebugPrint("[-] ObOpenObjectByPointer failed: 0x%X\n", status);
+				ObDereferenceObject(pEProcess);
+				break;
+			}
+
+			status = ZwUnmapViewOfSection(handle, ntdll);
+
+			if (NT_SUCCESS(status))
+			{
+				DebugPrint("[+] ZwUnmapViewOfSection succeeded for %p\n", ntdll);
+			}
+			else
+			{
+				DebugPrint("[-] ZwUnmapViewOfSection FAILED for %p -> Status: 0x%X\n", ntdll, status);
+			}
+
+			ZwClose(handle);
+			status = STATUS_SUCCESS;
 		}
-
-		status = ObOpenObjectByPointer(pEProcess, OBJ_KERNEL_HANDLE, NULL, 0x0008, *PsProcessType, KernelMode, &handle); //PROCESS_VM_OPERATION
-
-		if (!NT_SUCCESS(status)) 
+		else if (ControlCode == IO_CORRUPT_PEB)
 		{
-			DebugPrint("[-] ObOpenObjectByPointer failed: 0x%X\n", status);
-			ObDereferenceObject(pEProcess);
-			return status;
+			DWORD pid = *(DWORD*)Irp->AssociatedIrp.SystemBuffer;
+
+			DebugPrint("Control code 0x222 was passed with pid: %u\n", pid);
+
+
+			status = PsLookupProcessByProcessId((HANDLE)pid, &targetProcess);
+
+			if (!NT_SUCCESS(status))
+			{
+				DebugPrint("Failed to get EPROCESS\n");
+				break;
+			}
+
+			PPEB peb = PsGetProcessPeb(targetProcess);
+
+			PEB data = { 0 };
+			SIZE_T bytes = 0;
+
+			status = MmCopyVirtualMemory(PsGetCurrentProcess(), &data, targetProcess, peb, sizeof(PEB), KernelMode, &bytes);
+
+			if (NT_SUCCESS(status))
+			{
+				DebugPrint("MmCopyVirtualMemory succeeded -> 0x%X\n", peb);
+			}
+			else
+			{
+				DebugPrint("MmCopyVirtualMemory failed 0x%X\n", status);
+				break;
+			}
+			status = STATUS_SUCCESS;
 		}
-
-		status = ZwUnmapViewOfSection(handle, ntdll);
-
-		if (NT_SUCCESS(status)) 
+		else if (ControlCode == IO_CORRUPT_STACK)
 		{
-			DebugPrint("[+] ZwUnmapViewOfSection succeeded for %p\n", ntdll);
-		}
-		else 
-		{
-			DebugPrint("[-] ZwUnmapViewOfSection FAILED for %p -> Status: 0x%X\n", ntdll, status);
-		}
+			DWORD pid = *(DWORD*)Irp->AssociatedIrp.SystemBuffer;
 
-		ZwClose(handle);
-		status = STATUS_SUCCESS;
+			DebugPrint("Control code 0x333 was passed with pid: %u\n", pid);
+
+			status = PsLookupProcessByProcessId((HANDLE)pid, &targetProcess);
+
+			if (!NT_SUCCESS(status))
+			{
+				DebugPrint("Failed to get EPROCESS\n");
+				break;
+			}
+
+			HANDLE threadHandle = NULL;
+
+			status = ZwGetNextThread(targetProcess, NULL, THREAD_QUERY_LIMITED_INFORMATION | THREAD_GET_CONTEXT, 0, 0, &threadHandle);
+
+			if (!NT_SUCCESS(status))
+			{
+				DebugPrint("ZwGetNextThread failed: 0x%X", status);
+			}
+
+			/*
+					PETHREAD thread = NULL;
 
 
+					status = ObReferenceObjectByHandle(threadHandle, 0x0040, *PsThreadType, KernelMode, (PVOID*)&thread, NULL);
 
 
-	}
-	else if (ControlCode == IO_CORRUPT_PEB)
-	{
-		DWORD pid = *(DWORD*)Irp->AssociatedIrp.SystemBuffer;
+					if (!NT_SUCCESS(status))
+					{
+						DebugPrint("ObReferenceObjectByHandle failed: 0x%X", status);
+					}
 
-		DebugPrint("Control code 0x222 was passed with pid: %u\n", pid);
+					PVOID teb = PsGetThreadTeb(thread);
 
+					if (teb == NULL)
+					{
+						DebugPrint("teb is NULL\n");
 
-		status = PsLookupProcessByProcessId((HANDLE)pid, &targetProcess);
+					}
 
-		if (!NT_SUCCESS(status))
-		{
-			DebugPrint("Failed to get EPROCESS\n");
-			return status;
-		}
+					PVOID stackBase  = *(PVOID*)((PUCHAR)teb + 0x8);   // TEB->StackBase
+					PVOID stackLimit = *(PVOID*)((PUCHAR)teb + 0x10);  // TEB->StackLimit
 
-		PPEB peb = PsGetProcessPeb(targetProcess);
+					SIZE_T stackSize = (SIZE_T)stackLimit - (SIZE_T)stackBase;
 
-		PEB data = { 0 };
-		SIZE_T bytes = 0;
+					DebugPrint("TEB: %p  Stack: %p - %p  Stack size: %zu\n", teb, stackLimit, stackBase, stackSize);
 
-		status = MmCopyVirtualMemory(PsGetCurrentProcess(), &data, targetProcess, peb, sizeof(PEB), KernelMode, &bytes);
+					//PVOID buffer = ExAllocatePoolWithTag(NonPagedPoolNx, stackSize, 'pool');
+					//SIZE_T bytes = 0;
 
-		if (NT_SUCCESS(status))
-		{
-			DebugPrint("MmCopyVirtualMemory succeeded -> 0x%X\n", peb);
+					//if (!buffer)
+					//{
+					//	DebugPrint("Failed to allocate pool\n");
+					//  status = STATUS_UNSUCCESSFUL;
+					//  break;
+					//}
+
+					//memset(buffer, 0, stackSize);
+
+					//status = MmCopyVirtualMemory(PsGetCurrentProcess(), &buffer, targetProcess, stackBase, stackSize, KernelMode, &bytes);
+
+					//if (!NT_SUCCESS(status))
+					//{
+					//	DebugPrint("MmCopyVirtualMemory failed 0x%X\n", status);
+					//}
+
+					//ExFreePool(buffer);
+					//ObDereferenceObject(thread);
+
+					break;
+			*/
+			status = STATUS_SUCCESS;
 		}
 		else
 		{
-			DebugPrint("MmCopyVirtualMemory failed 0x%X\n", status);
-			return status;
+			DebugPrint("Unknown control code\n");
+
+			ByteIo = 0;
 		}
-		status = STATUS_SUCCESS;
 	}
-	else if (ControlCode == IO_CORRUPT_STACK)
-	{
+	while (FALSE);
 
-
-		DWORD pid = *(DWORD*)Irp->AssociatedIrp.SystemBuffer;
-		
-		DebugPrint("Control code 0x333 was passed with pid: %u\n", pid);
-
-		status = PsLookupProcessByProcessId((HANDLE)pid, &targetProcess);
-
-		if (!NT_SUCCESS(status))
-		{
-			DebugPrint("Failed to get EPROCESS\n");
-			return status;
-		}
-
-		
-		HANDLE threadHandle = NULL;
-
-		status = ZwGetNextThread(targetProcess, NULL, 0x0040 | 0x0008, 0, 0, &threadHandle); // (THREAD_QUERY_INFORMATION | THREAD_GET_CONTEXT)
-
-		if (!NT_SUCCESS(status))
-		{
-			DebugPrint("ZwGetNextThread failed: 0x%X", status);
-		}
-
-
-		PETHREAD thread = NULL;
-
-
-		status = ObReferenceObjectByHandle(threadHandle, 0x0040, *PsThreadType, KernelMode, (PVOID*)&thread, NULL);
-
-
-		if (!NT_SUCCESS(status))
-		{
-			DebugPrint("ObReferenceObjectByHandle failed: 0x%X", status);
-		}
-
-		PVOID teb = PsGetThreadTeb(thread);
-
-		if (teb == NULL)
-		{
-			DebugPrint("teb is NULL\n");
-
-		}
-
-		PVOID stackBase  = *(PVOID*)((PUCHAR)teb + 0x8);   // TEB->StackBase
-		PVOID stackLimit = *(PVOID*)((PUCHAR)teb + 0x10);  // TEB->StackLimit
-
-		SIZE_T stackSize = (SIZE_T)stackLimit - (SIZE_T)stackBase;
-
-		DebugPrint("TEB: %p  Stack: %p - %p  Stack size: %zu\n", teb, stackLimit, stackBase, stackSize);
-
-		//PVOID buffer = ExAllocatePoolWithTag(NonPagedPoolNx, stackSize, 'pool');
-		//SIZE_T bytes = 0;
-
-		//if (!buffer)
-		//{
-		//	DebugPrint("Failed to allocate pool\n");
-		//	return STATUS_UNSUCCESSFUL;
-		//}
-
-		//memset(buffer, 0, stackSize);
-
-		//status = MmCopyVirtualMemory(PsGetCurrentProcess(), &buffer, targetProcess, stackBase, stackSize, KernelMode, &bytes);
-
-		//if (!NT_SUCCESS(status))
-		//{
-		//	DebugPrint("MmCopyVirtualMemory failed 0x%X\n", status);
-		//}
-
-		//ExFreePool(buffer);
-		//ObDereferenceObject(thread);
-		return status;
-	}
-	else
-	{
-		DebugPrint("Unknown control code\n");
-
-		ByteIo = 0;
-	}
-
-
-	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Status = status;
 	Irp->IoStatus.Information = 0;
 
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
 	return status;
 }
-
-
-
 
 PVOID GetNtDll(DWORD pid)
 {
